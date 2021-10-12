ae1508c3993be539aa8de3849fff80b1298b33b6
Ricu Ruotsinsalo <thericu@gmail.com>
2021-10-12 13:05:19 +0300

    Noppa funktiot

--- java.js
+++ java.js
@@ -2,3 +2,37 @@
 // hyvin iso meemi :))))
 'use strict';
 
+//D&D dice roller
+
+function rollDice(){
+ return Math.random();
+}
+
+function d4(){
+return 1 + Math.floor(rollDice()*4);
+}
+function d6(){
+  return 1 + Math.floor(rollDice()*6);
+}
+function d8(){
+  return 1 + Math.floor(rollDice()*8);
+}
+function d10(){
+  return 1 + Math.floor(rollDice()*10);
+}
+function d12(){
+  return 1 + Math.floor(rollDice()*12);
+}
+function d20(){
+  return 1 + Math.floor(rollDice()*20);
+}
+function d100(){
+  return 1 + Math.floor(rollDice()*100);
+}
+console.log(d4());
+console.log(d6());
+console.log(d8());
+console.log(d10());
+console.log(d12());
+console.log(d20());
+console.log(d100());
