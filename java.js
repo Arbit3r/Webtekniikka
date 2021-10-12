1f8f7168eec4a3fba322ca04325bbdcc3c9c834a
Arbit3r <92163974+Arbit3r@users.noreply.github.com>
2021-10-13 02:44:07 +0300

    Main update

---
+++ java.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+// hyvin iso meemi :))))
+'use strict';
+
+//Navigointi linkit
+function over(obj){obj.style.color = "#e71d36ff";}
+function out(obj) {obj.style.color = "black";}
+
+// Koneen valinta Random
+function kone() {
+  	let x = document.getElementById("selected").value;
+	console.log(x);
+}
